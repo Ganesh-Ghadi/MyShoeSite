2045622a5b44b652aaafb6c96911327da4048e33
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2023-11-15 14:55:23 +0530

    sql file deleted

--- shoe-order/partials-front/menu.php
+++ shoe-order/partials-front/menu.php
@@ -20,7 +20,7 @@
           <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #ff3636;">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src="images/logo2.jpeg" width="80px;" alt="logo of a shoe">
+          <img class="rounded" src="images/logo2.jpeg" width="80px;" alt="logo of a shoe">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
